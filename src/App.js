faf43aceaee2e80af19292774d050d450f65aa6a
kanikas01 <larvakurt@yahoo.com>
2019-04-23 20:15:28 -0700

    Tweak basic App and index files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './NavBar.js';
+import Footer from './Footer.js';
+import Header from './Header.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Header />
+      <main className="container">
+        Content
+      </main>
+      <Footer />
     </div>
   );
 }
