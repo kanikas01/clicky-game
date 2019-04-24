223ca4301a66ae4b4c6f691befb8942139d3bb82
kanikas01 <larvakurt@yahoo.com>
2019-04-24 11:23:26 -0700

    Remove id tag

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
   return (
-    <header className="header" id="particles.js">
+    <header className="header">
       <h1>Clicky Game!</h1>
       <h2>Click on an image to earn points, but don't click on any more than once!</h2>
     </header>
