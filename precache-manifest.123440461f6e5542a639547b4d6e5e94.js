c4a0748f5248ea7cff24105b6bb0caf0ad8ff8bc
kanikas01 <larvakurt@yahoo.com>
2019-04-25 14:18:47 -0700

    Updates

---
+++ precache-manifest.123440461f6e5542a639547b4d6e5e94.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e6b3f8a779deb2c12e3974689e57f8",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/clicky-game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "20dd8c4da3f53819cdc1",
+    "url": "/clicky-game/static/js/main.907355ea.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
