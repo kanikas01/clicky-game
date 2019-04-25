047d601c366c89b87c1e959bfe500ddff22297ae
kanikas01 <larvakurt@yahoo.com>
2019-04-25 08:46:34 -0700

    Add render and shuffle function

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -8,19 +8,47 @@ class GameContainer extends Component {
     clicked: []
   };
 
-  handleIncrement = () => {
-    this.setState({ count: this.state.score + 1 });
+  handleIncrement = (e) => {
+    this.setState({ score: this.state.score + 1 });
   };
 
   handleReset = () => {
-    this.setState({ count: 0 });
+    this.setState({ score: 0 });
+  };
+
+  shuffle = (array) => {
+    let currentIndex = array.length;
+    let temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+
   };
 
   render () {
     return (
       <div className="container">
-      {this.props.images.map(image => (
-        <div role="img" aria-label="click item" className="click-item" key={image.id} style={{ backgroundImage: `url(${image.url})`}}></div>
+        {this.state.score}<br />
+      {this.shuffle(this.props.images).map(image => (
+        <div 
+          role="img" 
+          aria-label="click item" 
+          className="click-item" 
+          key={image.id}
+          onClick={this.handleIncrement}
+          style={{ backgroundImage: `url(${image.url})`}}>
+        </div>
       ))}
       </div>
     );
